906021980cb4e1673c55ec221bca51c99de6f167
YunKukPark <a01043340999@gmail.com>
2023-03-02 02:08:08 +0900

    ✅first render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('📍 Count는 0부터 시작합니다', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용하여 원하는 element에 접근(접근할 때 ID로)
+  const countElement = screen.getByTestId('counter');
+  // id가 counter인 element의 텍스트가 0인지 테스트
+  expect(countElement).toHaveTextContent('0');
 });
